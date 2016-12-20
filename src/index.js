dc0cc6e7446c0766afa54427e87fe19ff1addd34
BAlex <Alexander.Braunreuther@googlemail.com>
2016-12-20 14:52:50 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ApiError from './ApiError';
 import NotFoundError from './NotFoundError';
 import NotImplementedError from './NotImplementedError';
 import UnauthorizedError from './UnauthorizedError';
+import BadRequestError from './BadRequestError';
 
-export { handler, ApiError, NotFoundError, NotImplementedError, UnauthorizedError };
+export { handler, ApiError, NotFoundError, NotImplementedError, UnauthorizedError, BadRequestError };
